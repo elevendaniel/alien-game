6a66aed7964e0d89323ed3f812d9ea2411ff15a7
danielliu <danielliu@DanieldeMacBook-Pro.local>
2019-03-12 20:46:01 +0800

    v2 background color

--- game1.py
+++ game1.py
@@ -7,12 +7,21 @@ def run_game():
 	screen = pygame.display.set_mode((1200, 800))
 	pygame. display.set_caption("alien invasion")
 
+	#更换背景颜色
+	bg_color = (230, 230, 230)
+
 	#开始游戏的主循环
 
 	while True:
+
+		#监测键盘和鼠标事件
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys. exit()
+
+		#每次循环时都重新绘制屏幕
+		screen.fill(bg_color)
+
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
 
