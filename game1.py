ba2acd705999b0e3809b265e8ca5e123f11e5e6c
danielliu <danielliu@DanieldeMacBook-Pro.local>
2019-03-13 20:52:53 +0800

    add a ship

--- game1.py
+++ game1.py
@@ -1,7 +1,9 @@
 import sys 
 import pygame 
 
+
 from settings import Settings
+from ship import Ship #引入 ship中的ship功能
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
@@ -11,6 +13,9 @@ def run_game():
 		(ai_settings.screen_width, ai_settings.screen_height))
 	#screen = pygame.display.set_mode((1200, 800))
 	pygame. display.set_caption("alien invasion")
+
+	#创建一艘飞船
+	ship = Ship(screen)
 
 	#更换背景颜色
 	bg_color = (230, 230, 230)
@@ -26,6 +31,9 @@ def run_game():
 
 		#每次循环时都重新绘制屏幕
 		screen.fill(ai_settings.bg_color)
+
+		ship.blitme()
+		
 		#screen.fill(bg_color)
 
 		#让最近绘制的屏幕可见
